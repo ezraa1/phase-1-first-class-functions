27b7009a4393978f2638e2d0af2beef46a036b9b
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-06-08 23:11:30 +0300

    commit tree 4d2453dff1809065fbed810e904caaa1054173ce

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    return callback();
+}
+function returnsANamedFunction() {
+    var fn;
+    fn = () =>{}
+    return fn
+}
+function returnsAnAnonymousFunction(){ 
+    return (function() {
+        return ''
+    })
+}
